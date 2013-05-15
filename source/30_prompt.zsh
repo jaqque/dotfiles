9d44c2852fe8be03541cf6d5bed79ae23c12b2ce
John H. Robinson, IV <jaqque@gmail.com>
2013-05-15 13:50:24 -0700

    Integrate zsh-git-prompt, for real

--- source/30_prompt.zsh
+++ source/30_prompt.zsh
@@ -1,5 +1,12 @@
-# Use liquidprompt
-
-# TODO: Merge zsh-git-prompt into liquidprompt
+# Use liquidprompt, with zsh-git-prompt
 
 source ~/.dotfiles/libs/liquidprompt/liquidprompt
+source ~/.dotfiles/libs/zsh-git-prompt/zshrc.sh
+
+__GIT_PROMPT_DIR=~/.dotfiles/libs/zsh-git-prompt
+ZSH_THEME_GIT_PROMPT_NOCACHE=1
+
+# replace Liquid prompt's git with zsh-git-prompt
+_lp_git_branch_color () {
+  git_super_status
+}
